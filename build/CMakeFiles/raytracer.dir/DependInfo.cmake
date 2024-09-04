62723e137d79f148ba4d145cba92d6cbbc9817ef
bbbpimasheep <tadatatsuno@gmail.com>
2024-09-05 02:09:02 +0800

    new cbox renderer

---
+++ build/CMakeFiles/raytracer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/shizuku/Documents/Coding Sōko/raytracer/source/bounds.cpp" "CMakeFiles/raytracer.dir/source/bounds.cpp.o" "gcc" "CMakeFiles/raytracer.dir/source/bounds.cpp.o.d"
+  "/Users/shizuku/Documents/Coding Sōko/raytracer/source/image.cpp" "CMakeFiles/raytracer.dir/source/image.cpp.o" "gcc" "CMakeFiles/raytracer.dir/source/image.cpp.o.d"
+  "/Users/shizuku/Documents/Coding Sōko/raytracer/source/interval.cpp" "CMakeFiles/raytracer.dir/source/interval.cpp.o" "gcc" "CMakeFiles/raytracer.dir/source/interval.cpp.o.d"
+  "/Users/shizuku/Documents/Coding Sōko/raytracer/source/main.cpp" "CMakeFiles/raytracer.dir/source/main.cpp.o" "gcc" "CMakeFiles/raytracer.dir/source/main.cpp.o.d"
+  "/Users/shizuku/Documents/Coding Sōko/raytracer/source/shapes.cpp" "CMakeFiles/raytracer.dir/source/shapes.cpp.o" "gcc" "CMakeFiles/raytracer.dir/source/shapes.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
